33b835ddf89221685b5bfa80d76299a274afa3aa
BeikSant <beiker.santorum@unl.edu.ec>
2023-08-11 21:19:33 +0000

    Socket prod

--- src/boot/socket.js
+++ src/boot/socket.js
@@ -1,7 +1,11 @@
 import { boot } from 'quasar/wrappers'
 import io from 'socket.io-client'
 
-const socket = io(process.env.API_URL)
+const socket = io(process.env.API_URL, {
+  transports: ["polling"],
+  path: '/informefinal-backend/socket.io/',
+  forceNew: true
+})
 
 export default boot(({ app }) => {
   app.config.globalProperties.$socket = socket
